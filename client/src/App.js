00f99ed8ac0a97a004b2764752ac6b012e85492e
Chrissypike <you@example.com>
2023-04-11 21:25:16 -0500

    cleaning up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import SavedNotes from './pages/SavedNotes';
 import Navbar from './components';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
+          <Route exact path='/saved' component={SavedNotes} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
