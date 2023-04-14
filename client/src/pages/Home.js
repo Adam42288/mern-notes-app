b58cd212e4c4b19631aca0b0b99ce6c2a2786ced
danicaon <danica_escot@hotmail.com>
2023-04-13 19:16:01 -0500

    adding buttons on login pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Signup from "./SignupForm";
 import Login from "./LoginForm";
+import ForgotPassword from "./ForgotPass"
 
 const Home = () => {
   return (
     <div className="container">
       <Signup />
       <Login />
+      <ForgotPassword/>
     </div>
   );
 };
