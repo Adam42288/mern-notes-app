28ecd02d126c5a8484c00257080ac2d48ec588ad
danicaon <danica_escot@hotmail.com>
2023-04-16 18:09:36 -0500

    Stylying for LoginForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-
-import Home from './pages/Home';
-import Login from './pages/LoginForm';
-import Signup from './pages/SignupForm';
-import Navbar from './components/Navbar';
+import React, { useState } from "react";
+import './App.css';
+import { Login } from "./pages/LoginForm";
+import { Signup } from "./pages/SignupForm";
 
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
 
-
-//TODO: Still need to add forgot password and the Note.js when code is written
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route 
-            path='/' 
-            element={<Home />}
-            />
-             <Route 
-            path='/loginForm' 
-            element={<Login />}
-            />
-             <Route 
-            path='/SignupForm'
-            element={<Signup />} 
-            />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Signup onFormSwitch={toggleForm} />
+      }
     </div>
-  )
+  );
 }
- export default App;
+
+export default App;
