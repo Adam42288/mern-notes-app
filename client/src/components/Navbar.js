0dfda6fac63d2b67a4a8539f2ce3f11ed956b908
mollyturner <mollyturner50@gmail.com>
2023-04-17 18:19:13 -0500

    Add style to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const style = {
+        position: "absolute",
+        top: "0"
+        
+    }
+
     return (
-        <header>
+        <header style={style}>
             <div className='container'>
                 <Link to="/">
                     <h1>Secret Keeper</h1>
