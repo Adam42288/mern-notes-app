69b6ecebd96d9417551e459569e062f341e9f95b
Chrissypike <you@example.com>
2023-04-16 19:09:17 -0500

    fixing merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
 
 import React from 'react';
-import React, { useState } from "react";
 import './App.css';
-import { Login } from "./pages/LoginForm";
-import { Signup } from "./pages/SignupForm";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Note from './pages/Note';
+import Login from "./pages/LoginForm";
+import Signup from "./pages/SignupForm";
 
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
+  
 
   return (
     <div className="App">
@@ -27,13 +22,16 @@ function App() {
         <div className="pages">
           <Routes>
             <Route 
-              {
-                currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Signup onFormSwitch={toggleForm} />
-              }
-            />
-            <Route 
             path='/' 
             element={<Home />}
+            />
+             <Route 
+            path='/LoginForm' 
+            element={<Login />}
+            />
+             <Route 
+            path='/SignupForm' 
+            element={<Signup />}
             />
              <Route 
             path='/Note'
