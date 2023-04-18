9cc5fba359738df65b00d9bc7d6f28da56f241a5
mollyturner <mollyturner50@gmail.com>
2023-04-18 01:13:01 -0500

    Add entry mutation and query. Adjust styling of book.

--- client/src/pages/Note.jsx
+++ client/src/pages/Note.jsx
@@ -1,8 +1,11 @@
-import { Box, Fab, Fade, Paper, TextField, Typography } from '@mui/material';
+import { Box, Fab, Fade, TextField, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import EditIcon from '@mui/icons-material/Edit';
 import HTMLFlipBook from 'react-pageflip';
 import React, { useRef, useState } from 'react';
+import { useQuery, useMutation } from "@apollo/client"
+import { QUERY_ME } from '../utils/queries';
+import { ADD_NOTE } from '../utils/mutations';
 
 const useStyles = makeStyles((theme) => ({
   page: {
@@ -11,11 +14,15 @@ const useStyles = makeStyles((theme) => ({
     color: '#785e3a',
     border: '1px solid #c2b5a3',
     // overflow: 'hidden',
-    fontSize: '80%',
+    fontSize: '200px',
   },
   pageBorderRight: {
     borderLeft: 0,
     boxShadow: 'inset 7px 0 30px -7px rgba(0,0,0,.4)',
+  },
+  pages: {
+    height: "100vh",
+    width: "100vw"
   },
   pageBorderLeft: {
     borderLeft: 0,
@@ -45,11 +52,33 @@ const savedNotes = [
   const flipBook = useRef();
   const classes = useStyles();
   const [showTextBox, setShowTextBox] = useState(false);
+  const [newNote, setNewNote] = useState('')
+
+  const { data } = useQuery(QUERY_ME)
+
+  const [addNote] = useMutation(ADD_NOTE)
+
+  console.log("checking user", data?.me)
 
   const toggleTextBox = () => {
     setShowTextBox(!showTextBox);
   };
 
+  console.log(showTextBox)
+  const changeHandler = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const saveNote = async () => {
+    const response = await addNote({
+      refetchQueries: [
+        {query: QUERY_ME}
+      ],
+      variables: {
+        noteText: newNote
+      }
+    })
+  }
 
   return (
     <Box
@@ -68,41 +97,46 @@ const savedNotes = [
       }}
     >
       <HTMLFlipBook
-        width={300}
-        height={600}
+        width={500}
+        height={800}
         showPageCorners={true}
         ref={flipBook}
+        style={{marginTop: "15vh"}}
       >
-        {savedNotes?.map((note, index) => (
-          <div className={`${classes.page} ${classes.pageBorderRight}`}>
+        {data?.me.notes.map((note, index) => (
+          <div key={index} className={`${classes.page} ${classes.pageBorderRight}`}>
             <Typography variant="h4" sx={{ padding: " 10px 20px", textAlign: 'center', marginBottom: '1rem' }}>
-              Page {index + 1}
+              Entry {index + 1}
             </Typography>
 
-            <Typography variant="body1" sx={{ padding: " 10px 20px", textAlign: 'center', fontSize: '.75rem' }}>
-              {note}
+            <Typography variant="body1" sx={{ padding: " 10px 20px", textAlign: 'center', fontSize: '1.2rem' }}>
+              {note?.noteText}
             </Typography>
           </div>
         ))}
       </HTMLFlipBook>
       <Box className={classes.controlBox}>
         {/*Text box*/}
-        <Fade
-          in={showTextBox}
+        <Box
+          style={showTextBox ? {display: "block"} : {display: "none"}}
         >
+          <>
           <TextField
           sx={{ margin: '1em 0' }}
             className={classes.inputTextBox}
             id="filled-basic"
-            label="Filled"
+            label="Thoughts...?"
             variant="filled"
             multiline
+            onChange={changeHandler}
           />
-        </Fade>
+          <button style={{margin: "15px"}} onClick={saveNote}>Save Note</button>
+          </>
+        </Box>
 
         {/*Pink button*/}
         <Fab
-          sx={{ backgroundColor: '#ffb3c6' }}
+          sx={{ backgroundColor: '#ffb3c6', margin: "20px"}}
           variant="extended"
           onClick={toggleTextBox}
         >
